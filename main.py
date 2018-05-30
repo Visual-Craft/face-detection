96040f7825120d90a9bfbf643945911d5472dd55
Anton Bakai <inso@i.ua>
2018-05-30 18:41:12 +0300

    refactored error handling

--- main.py
+++ main.py
@@ -2,14 +2,43 @@ import os
 import face_recognition
 from flask import Flask, request, abort, jsonify
 from tempfile import mkstemp
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
+
+
+@app.errorhandler(HTTPException)
+def error_handler(error):
+    code = error.code if error.code is not None else 500
+    data = {
+        "error": error.name,
+        "code": code,
+    }
+
+    if error.description is not None:
+        data["description"] = error.description
+
+    response = jsonify(data)
+    response.status_code = code
+
+    return response
+
+
+@app.errorhandler(Exception)
+def error_handler(error):
+    response = jsonify({
+        "error": "Internal Server Error",
+        "code": 500,
+    })
+    response.status_code = 500
+
+    return response
 
 
 @app.route('/detect-faces', methods=['POST'])
 def detect_faces():
     if 'image' not in request.files:
-        abort(400)
+        abort(400, description1="missing 'image' POST field")
 
     descriptor, path = mkstemp()
     os.close(descriptor)
@@ -20,7 +49,7 @@ def detect_faces():
         try:
             image = face_recognition.load_image_file(path)
         except OSError:
-            abort(400)
+            abort(400, description="unable to load image")
 
         face_locations = face_recognition.face_locations(image)
     finally:
